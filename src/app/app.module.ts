9a92c82431e757b5fa2f01a843a660663b377f11
Re <515494526@qq.com>
2020-01-03 20:05:45 +0800

    增加tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './content/main-content/top-bar/top-bar.component';
-import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
 import { ContentComponent } from './content/content.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import { LeftContentComponent } from './content/left-content/left-content.component';
 import { MainContentComponent } from './content/main-content/main-content.component';
+import { MainContentTabComponent } from './content/main-content/main-content-tab/main-content-tab.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +25,8 @@ import { MainContentComponent } from './content/main-content/main-content.compon
     TopBarComponent,
     ContentComponent,
     LeftContentComponent,
-    MainContentComponent
+    MainContentComponent,
+    MainContentTabComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +37,8 @@ import { MainContentComponent } from './content/main-content/main-content.compon
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/left-content/left-content.component.ts
+++ src/app/content/left-content/left-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContentTabService} from '../content-tab.service';
 
 @Component({
   selector: 'app-left-content',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftContentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contentTabService: ContentTabService) {
+  }
 
   ngOnInit() {
   }
 
+  addTab() {
+    this.contentTabService.addTab('new');
+  }
 }
