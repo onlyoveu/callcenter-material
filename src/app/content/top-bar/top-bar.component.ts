e4b2c170a10519f0d7c1afe50afd2ae2539eb309
刘佳宇_翰声_外呼中心 <jiayu_liu@hiyees.com>
2020-09-08 16:04:39 +0800

    增加注册功能

--- src/app/content/top-bar/top-bar.component.ts
+++ src/app/content/top-bar/top-bar.component.ts
@@ -4,6 +4,8 @@ import {interval, of, Subscription} from 'rxjs';
 import {delay} from 'rxjs/operators';
 import {PhoneNumService} from '../phone-num.service';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {SimpleUser, SimpleUserDelegate, SimpleUserOptions} from 'sip.js/lib/platform/web';
+import {UserAgent} from 'sip.js';
 
 const CALLER = '10016';
 const AGENT_STATE = {
@@ -16,6 +18,8 @@ const AGENT_STATE = {
 };
 
 export interface AgentInfo {
+  // wss服务器地址
+  wssServer: string;
   // 账号
   username: string;
   // 姓名
@@ -25,7 +29,6 @@ export interface AgentInfo {
   // 密码
   pwd: string;
 }
-
 
 @Component({
   selector: 'app-top-bar',
@@ -43,11 +46,38 @@ export class TopBarComponent implements OnInit, OnDestroy {
 
   phoneNumSubjection: Subscription;
 
+  /* 坐席信息 */
   agentInfo: AgentInfo = {
+    wssServer: '',
     username: '',
     name: '',
     workno: '',
     pwd: ''
+  };
+  /* user agent */
+  simpleUser: SimpleUser;
+  // SimpleUser delegate
+  simpleUserDelegate: SimpleUserDelegate = {
+    onCallCreated: (): void => {
+      console.log(`[${this.agentInfo.workno}] Call created`);
+
+    },
+    onCallAnswered: (): void => {
+      console.log(`[${this.agentInfo.workno}] Call answered`);
+
+    },
+    onCallHangup: (): void => {
+      console.log(`[${this.agentInfo.workno}] Call hangup`);
+
+    },
+    onCallHold: (held: boolean): void => {
+      console.log(`[${this.agentInfo.workno}] Call hold ${held}`);
+
+    },
+    onCallReceived: (): void => {
+      console.log(`[${this.agentInfo.workno}] received Call`);
+      this.answerCall();
+    }
   };
 
   /* 搜索框手机号表单 */
@@ -202,9 +232,7 @@ export class TopBarComponent implements OnInit, OnDestroy {
     console.log(`set agent ${this.agentStateCode}, ${this.agentStateName}`);
   }
 
-  /**
-   * 呼出
-   */
+  /* 呼出 */
   callOut() {
     if (this.agentStateCode === 1) {
       /*设置主被叫*/
@@ -215,6 +243,7 @@ export class TopBarComponent implements OnInit, OnDestroy {
       this.updateStateCodeAndName(3);
       /*设置外拨按钮*/
       this.callOutBtnName = '挂机';
+      this.makeCall();
     } else if (this.agentStateCode === 3) {
       /*设置主被叫*/
       this.caller = '';
@@ -224,9 +253,11 @@ export class TopBarComponent implements OnInit, OnDestroy {
       this.updateStateCodeAndName(5);
       /*设置外拨按钮*/
       this.callOutBtnName = '外拨';
+      this.hangupCall();
     }
   }
 
+  /* 保持 */
   keep() {
     if (this.agentStateCode === 4) {
       this.updateStateCodeAndName(3);
@@ -237,25 +268,112 @@ export class TopBarComponent implements OnInit, OnDestroy {
     }
   }
 
-  /* 注册 */
+  /** 注册 */
   openRegisterDialog(): void {
     const matDialogRef = this.dialog.open(RegisterDialogComponent, {
-      width: '250px',
+      width: '300px',
       data: {
-        workno: '10002',
+        wssServer: 'wss://192.168.200.21:7443',
+        workno: '1002',
         pwd: '1234'
       }
     });
     matDialogRef.afterClosed().subscribe(result => {
-      console.log(`register ${result}`);
-      this.agentInfo.workno = result;
+      console.log(`register ${JSON.stringify(result)}`);
+      if (result) {
+        /* 注册信息 */
+        const myURI = 'sip:' + result.workno + '@' + result.wssServer.replace('wss://', '');
+        const simpleUserOptions: SimpleUserOptions = {
+          aor: myURI,
+          delegate: this.simpleUserDelegate,
+          media: {
+            remote: {
+              audio: this.getAudio('remoteAudio')
+            }
+          },
+          userAgentOptions: {
+            authorizationUsername: result.workno,
+            authorizationPassword: result.pwd,
+            displayName: result.workno,
+            uri: UserAgent.makeURI(myURI)
+          }
+        };
+        this.simpleUser = new SimpleUser(result.wssServer, simpleUserOptions);
+        /* 连接websocket */
+        this.simpleUser
+          .connect()
+          .then(() => {
+            console.log(`[${this.simpleUser.id}] connect success`);
+            /* 连接成功后注册 */
+            this.simpleUser
+              .register()
+              .then(() => {
+                this.agentInfo.wssServer = result.wssServer;
+                this.agentInfo.workno = result.workno;
+                this.agentInfo.pwd = result.pwd;
+                console.log(`[${this.simpleUser.id}] register success, register info=${JSON.stringify(result)}, angentInfo=${JSON.stringify(this.agentInfo)}`);
+              })
+              .catch((error: Error) => {
+                console.error(`[${this.simpleUser.id}] failed to register`);
+                console.error(error);
+                alert(`[${this.simpleUser.id}] failed to register.\n` + error);
+              });
+          })
+          .catch((error: Error) => {
+            console.error(`[${this.simpleUser.id}] failed to connect`);
+            console.error(error);
+            alert('Failed to connect.\n' + error);
+          });
+      }
     });
+  }
+
+  makeCall(): void {
+    this.simpleUser.call(this.phoneNo.value)
+      .then(() => {
+        console.log(`[${this.simpleUser.id}] success to place call`);
+      })
+      .catch((error: Error) => {
+        console.error(`[${this.simpleUser.id}] failed to place call`);
+        console.error(error);
+        alert('Failed to place call.\n' + error);
+      });
+  }
+
+  answerCall(): void {
+    this.simpleUser
+      .answer()
+      .then(() => {
+        console.log(`[${this.simpleUser.id}] answer call success`);
+      })
+      .catch((error: Error) => {
+        console.error(`[${this.simpleUser.id}] answer call failed`);
+        console.error(error);
+        alert('answer call failed.\n' + error);
+      });
+  }
+
+  hangupCall(): void {
+    this.simpleUser.hangup().catch((error: Error) => {
+      console.error(`[${this.simpleUser.id}] failed to hangup call`);
+      console.error(error);
+      alert('Failed to hangup call.\n' + error);
+    });
+  }
+
+  getAudio(id: string): HTMLAudioElement {
+    const el = document.getElementById(id);
+    if (!(el instanceof HTMLAudioElement)) {
+      throw new Error(`Element "${id}" not found or not an audio element.`);
+    }
+    return el;
   }
 }
 
 @Component({
   selector: 'app-register-dialog',
-  templateUrl: './register-dialog.component.html'
+  templateUrl: './register-dialog.component.html',
+  styleUrls: ['./top-bar.component.css']
 })
 export class RegisterDialogComponent {
   constructor(
